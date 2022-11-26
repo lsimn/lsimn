d990acc3ab4ca6be86cce23311f059386a2a99d5
Administrator <Administrator@I-01>
2022-11-26 17:49:11 +0900

    dada

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+
+            #region 변수
+            //변수
+            ////{
+            //// 전투 시스템 = Main 함수 안 쪽에 넣어주셔야합니다.
+            ////
+            ////}
+
+            ////자료형이란
+            ////변수에 값을 저장하기 전에 정수인지, 문자인지, 실수인지 미리알려준는 명령어
+
+            ////변수란?
+            ////어떤 공간에 어떤 값을 저장하기 위한 이름을 가지는 공간입니다.
+            ////변수에 있는 값을 출력할때는 ""를 하지않아도 됨
+
+
+            ////char의 크기 : 1byte
+            ////표현할 수 있는범위 : -128 ~ 127
+
+            ////short의 크기 : 2byte
+            ////표현할 수 있는범위 : -32768 ~ 32767
+
+            ////int의 크기 : 4byte
+            ////표현할 수 있는범위 : -2147483648 ~ 2147483647
+
+            ////float의 크기 : 4byte
+
+            ////double의 크기 : 8byte
+
+            //char alphbet = 'A';
+            ////// 오버플로우란?
+            ////// 특정한 자료형이 표현할 수 있는 최댓값의
+            ////// 범위를 넘어서 연산을 수행하는 과정
+            ////short value = 50000;
+            ////// 특정한 자료형이 표현할 수 있는 최솟값의
+            ////// 범위를 넘어서 연산을 수행하는 과정
+            ////short variable = -50000;
+            //int health = 100;
+            //float attack = 50.5f;
+
+            //Console.WriteLine("alphbet의 값 : " + alphbet);
+            //Console.WriteLine("health의 값 : " + health);
+            //Console.WriteLine("attack의 값 : " + attack);
+            #endregion
+
+            #region 상수
+            //int a = 100;
+            //Console.WriteLine("a의 값: " + a);
+            //a = 300;
+            //Console.WriteLine("a의 값: " + a);
+
+            //const int b = 300;
+            //// 상수는 컴파일 시점에 현재 있는 값이
+            //// 고정되기 떄문에 값을 바꿀 수 없습니다.
+            ////b = 500;
+
+            ////리터럴 상수
+            ////메모리 공간을 가지고 있지 않은 상수입니다.
+
+            ////심볼릭 상수
+            ////메모리 공간을 가지고 있는 상수입니다.
+            #endregion 
+        }
+    }
+}
